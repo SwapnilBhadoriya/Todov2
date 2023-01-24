a24f21d1f104fdd450fa18fb52fabe99bd1bfe3d
SwapnilBhadoriya <swapnilbhadoriya10@gmai.com>
2023-01-24 18:12:33 +0530

    Added error message and loaders for login/signup & edit in user tasks

--- server/controllers/userHandler.js
+++ server/controllers/userHandler.js
@@ -2,29 +2,33 @@ const { pool } = require("../config/dbconfig");
 
 const getAllTodos = async (req, res) => {
   try {
-    const result = await pool.query("Select * from Todos where user_id=$1 order by task_id",[req.user.id]);
-    res.status(200).json({ success:true,data: result.rows });
+    const result = await pool.query(
+      "Select * from Todos where user_id=$1 order by task_id",
+      [req.user.id]
+    );
+    res.status(200).json({ success: true, data: result.rows });
   } catch (error) {
-    console.log("Error in fetching data from the database..",error);
+    console.log("Error in fetching data from the database..", error);
     res.status(500).json({ msg: "Server side Error", error });
   }
 };
 const addNewTodo = async (req, res) => {
   try {
     const addQuery = "Insert into Todos(task,user_id) Values($1,$2)";
-    const todo = [req.body.task,req.user.id];
+    const todo = [req.body.task, req.user.id];
     await pool.query(addQuery, todo);
-    res.status(201).json({ success:true,msg: "Successfully added the todo" });
+    res.status(201).json({ success: true, msg: "Successfully added the todo" });
   } catch (error) {
     res.status(500).json({ msg: "Server side Error", error });
   }
 };
 const getSingleTodo = async (req, res) => {
   try {
-    const result = await pool.query("Select * from Todos where user_id=$1 and task_id=$2", [req.user.id,
-      req.params.task_id,
-    ]);
-    res.status(200).json({ success:true,data: result.rows });
+    const result = await pool.query(
+      "Select * from Todos where user_id=$1 and task_id=$2",
+      [req.user.id, req.params.task_id]
+    );
+    res.status(200).json({ success: true, data: result.rows });
   } catch (error) {
     console.log("Error in getting data from the database.");
     res.status(500).json({ msg: "Server side Error", error });
@@ -33,7 +37,10 @@ const getSingleTodo = async (req, res) => {
 
 const deleteTodo = async (req, res) => {
   try {
-    await pool.query("Delete from Todos where task_id=$1 and user_id=$2", [req.params.task_id,req.user.id]);
+    await pool.query("Delete from Todos where task_id=$1 and user_id=$2", [
+      req.params.task_id,
+      req.user.id,
+    ]);
     res.status(204).send();
   } catch (error) {
     console.log("Error in deleting data from the database.");
@@ -43,9 +50,13 @@ const deleteTodo = async (req, res) => {
 
 const updateTodo = async (req, res) => {
   try {
-    const updates = [req.body.task, req.body.completed, req.params.task_id,req.user.id];
+    const updates = [
+      req.body.task,
+      req.params.task_id,
+      req.user.id,
+    ];
     await pool.query(
-      "Update Todos set task=$1,completed=$2 where task_id=$3 and user_id=$4",
+      "Update Todos set task=$1 where task_id=$2 and user_id=$3",
       updates
     );
     res.status(201).json({ msg: "Updation Successfull" });
@@ -55,5 +66,10 @@ const updateTodo = async (req, res) => {
   }
 };
 
-
-module.exports = {getAllTodos,getSingleTodo,deleteTodo,updateTodo,addNewTodo};
+module.exports = {
+  getAllTodos,
+  getSingleTodo,
+  deleteTodo,
+  updateTodo,
+  addNewTodo,
+};
